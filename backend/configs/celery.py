59e40a2d82088ce10013c1ab8ea3bde5bcca5be7
lubomyr <lubomyr1995@gmail.com>
2023-04-21 13:50:41 +0300

    add hw9

--- backend/configs/celery.py
+++ backend/configs/celery.py
@@ -10,9 +10,9 @@ app.config_from_object('django.conf.settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-app.conf.beat_schedule = {
-    "send_spam_every_minute": {
-        "task": "core.services.email_service.spam",
-        "schedule": crontab()
-    }
-}
+# app.conf.beat_schedule = {
+#     "send_spam_every_minute": {
+#         "task": "core.services.email_service.spam",
+#         "schedule": crontab()
+#     }
+# }
